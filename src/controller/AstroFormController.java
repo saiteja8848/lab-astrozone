b6cd765e3e65c88dbdc01756097d268d1497c7f4
saiteja8848 <saiteja8848@gmail.com>
2020-05-14 11:17:57 +0530

    FSJ0420A102

--- src/controller/AstroFormController.java
+++ src/controller/AstroFormController.java
@@ -2,6 +2,7 @@
 package controller;
 
 import java.io.IOException;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,10 +10,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import model.User;
+import service.AstroCalculator;
+
 
 @WebServlet(urlPatterns = {"/entervalue"})
 public class AstroFormController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private AstroCalculator astroCal;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/entervalues.jsp");
@@ -20,7 +25,7 @@ public class AstroFormController extends HttpServlet {
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-/* 
+ 
 		String name=request.getParameter("name");
 		String gender=request.getParameter("gender");
 		String date=request.getParameter("dob1");
@@ -28,66 +33,73 @@ public class AstroFormController extends HttpServlet {
 		String year=request.getParameter("dob3");
 		String dob = date+"-"+month+"-"+year;	
 		
+		User user = new User(name,dob,gender);
+		System.out.println("this method is called");
+		 astroCal = new AstroCalculator();
+		String astrosign = astroCal.findSign(user);
+		System.out.println("hello:"+astrosign); 
+		
 		if(astrosign.equals("Aquarius")){
 			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/Outputview.jsp");
 			request.setAttribute("astro", "<img src='./assets/aquarius.svg'>");
 			rd.forward(request, response);
 		}
 		
-		if(astrosign.equals("Aries")){
+		else if(astrosign.equals("Aries")){
 			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/Outputview.jsp");
 			request.setAttribute("astro", "<img src='./assets/aries.svg'>");
 			rd.forward(request, response);
 		}
-		if(astrosign.equals("Cancer")){
+		else if(astrosign.equals("Cancer")){
 			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/Outputview.jsp");
 			request.setAttribute("astro", "<img src='./assets/cancer.svg'>");
 			rd.forward(request, response);
 		}
-		if(astrosign.equals("Capricorn")){
+		else if(astrosign.equals("Capricorn")){
 			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/Outputview.jsp");
 			request.setAttribute("astro", "<img src='./assets/capricorn.svg'>");
 			rd.forward(request, response);
 		}
-		if(astrosign.equals("Gemini")){
+		else if(astrosign.equals("Gemini")){
 			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/Outputview.jsp");
 			request.setAttribute("astro", "<img src='./assets/gemini.svg'>");
 			rd.forward(request, response);
 		}
-		if(astrosign.equals("Leo")){
+		else if(astrosign.equals("Leo")){
 			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/Outputview.jsp");
 			request.setAttribute("astro", "<img src='./assets/leo.svg'>");
 			rd.forward(request, response);
 		}
-		if(astrosign.equals("Libra")){
+		else if(astrosign.equals("Libra")){
 			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/Outputview.jsp");
 			request.setAttribute("astro", "<img src='./assets/libra.svg'>");
 			rd.forward(request, response);
 		}
-		if(astrosign.equals("Pisces")){
+		else if(astrosign.equals("Pisces")){
 			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/Outputview.jsp");
 			request.setAttribute("astro", "<img src='./assets/pisces.svg'>");
 			rd.forward(request, response);
 		}
-		if(astrosign.equals("Sagittarius")){
+		else if(astrosign.equals("Sagittarius")){
 			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/Outputview.jsp");
 			request.setAttribute("astro", "<img src='./assets/sagittaruis.svg'>");
 			rd.forward(request, response);
 		}
-		if(astrosign.equals("Scorpio")){
+		else if(astrosign.equals("Scorpio")){
 			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/Outputview.jsp");
 			request.setAttribute("astro", "<img src='./assets/scorpio.svg'>");
 			rd.forward(request, response);
 		}
-		if(astrosign.equals("Taurus")){
+		else if(astrosign.equals("Taurus")){
 			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/Outputview.jsp");
 			request.setAttribute("astro", "<img src='./assets/taurus.svg'>");
 			rd.forward(request, response);
 		}
+		else 
 		if(astrosign.equals("Virgo")){
 			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/Outputview.jsp");
 			request.setAttribute("astro", "<img src='./assets/virgo.svg'>");
 			rd.forward(request, response);
-		}*/
+		}
 	}
 }
--- src/service/AstroCalculator.java
+++ src/service/AstroCalculator.java
@@ -1,3 +1,81 @@
 package service;
 
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+
+import model.User;
+
 // Type your code
+
+public class AstroCalculator{
+	
+	public String findSign(User user) {
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+		LocalDate Date = LocalDate.parse(user.getDob(), formatter);
+         int date = Date.getDayOfMonth();
+         int month = Date.getMonthValue();
+         int year =Date.getYear();
+         String result="";
+         
+        
+         System.out.println("date:"+date);
+         System.out.println("month:"+month);
+         
+		if((date>21 && month == 12) ||  (date<=19 && month == 1)){
+			result="Capricon";
+		}
+		else if((date>20 && month ==1) ||  (date<=18 && month == 2)){
+			result="Aquarius";
+		}
+		else if((date>19 && month == 2) || (date <=20 && month == 3)){
+			result="Pisces";
+	       }
+		else if ((date>21 && month == 3) || (date <=20 && month == 4)){
+	    	   result="Aries";
+	       }
+		else if ((date>21 && month == 4) || (date <=20 && month == 5)){
+	    	   result="Taurus";
+	       }
+		else if ((date>21 && month == 5) || (date<=20 && month == 6)){
+	    	   result="Gemini";
+	       }
+		else if ((date>21 && month == 6) || (date<=20 && month == 7)){
+	    	   result="Cancer";
+	       }
+		else if ((date>21 && month  == 7) || (date<=20 && month== 8)){
+	    	   result="Leo";
+	       }
+		else if ((date>21 && month ==8) || (date<=22 && month == 9)){
+	    	   result="Virgo";
+	       }
+		else if ((date>23 && month == 9) || (date<=20 && month ==10)){
+	    	   result="Libra";
+	       }
+		else if ((date>21 && month == 10) || (date<=22 && month == 11)){
+	    	   result="Scorpio";
+	       }
+		else if ((date>23 && month == 11) || (date<=20 && month == 12)){
+	    	   result="Sagitarius";
+	       }
+	       else
+	    	   result="please enter all the values";
+			
+		LocalDate today = LocalDate.now();
+		LocalDate birthday = LocalDate.of(year,date,month);
+		Period p =Period.between(birthday,today);
+		
+		
+		
+	       System.out.println("Your details");
+	   		System.out.println("Name: "+user.getName());
+	   		System.out.println("Age: "+p.getYears());
+	   		System.out.println("Your Astrology: "+result);
+	     
+       return result;
+	}
+	 
+	   
+	   	
+		
+}
